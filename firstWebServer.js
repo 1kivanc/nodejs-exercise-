24d8620e812661dffee55815372961b1e192c031
1kivanc <kivanc0556@gmail.com>
2023-02-06 22:16:50 +0300

    odev5

---
+++ firstWebServer.js
@@ -0,0 +1,30 @@
+const http = require('http');
+
+const server = http.createServer((req,res)=> {
+    
+    const url = req.url;
+    console.log(url);
+    if(url === '/'){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write('<h2>home page</h2>');
+    }else if(url === '/about'){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write('<h2>About page</h2>');
+    }else if(url == '/contact'){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write('<h2>contact page</h2>');
+    }
+    else {
+
+        res.writeHead(404,{'Content-Type':'text/html'});
+        res.write('<h2>404 page is not found</h2>');
+    }
+    res.end();
+
+});
+
+const port = 5000;
+
+server.listen(port,()=> {
+    console.log(`Sunucu port ${port} de başlatıldı.`)
+});
